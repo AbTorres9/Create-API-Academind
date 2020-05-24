9b9862a0111bc9b61862e752eaf641ed9cb849c3
AbTorres9 <you@example.com>
2020-05-24 09:42:00 +0530

    protecting routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 //USING MULTER TO UPLOAD IMAGES
 const multer = require('multer');
@@ -62,7 +63,7 @@ router.get("/", (req, res, next) => {
   });
  
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", upload.single('productImage'), checkAuth, (req, res, next) => {
     console.log(req.file)
     const product = new Product({
       _id: new mongoose.Types.ObjectId(),
@@ -123,7 +124,7 @@ router.get('/:productId', async (req, res, next)=> {
 
 
 
-router.patch('/:productId', async (req, res, next)=> {
+router.patch('/:productId', checkAuth, async (req, res, next)=> {
     const id= req.params.productId; 
     const props = req.body;
     try{
@@ -147,7 +148,7 @@ router.patch('/:productId', async (req, res, next)=> {
 });    
 
 
-router.delete('/:productId', (req, res, next)=> {
+router.delete('/:productId', checkAuth, (req, res, next)=> {
     const id= req.params.productId; 
     Product.remove({_id: id})
     .exec()
